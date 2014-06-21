ba99b52603cb9c5fc7f7b3132445afabd024bfc0
Lars Gregori <lars.gregori@gmail.com>
2014-06-21 14:34:04 +0200

    finish

--- twab.scm
+++ twab.scm
@@ -1,17 +1,3 @@
-;
-;
-;	Neuester twab.scm vom 07.11.2007
-;
-;
-;	Hi,
-;
-;	ich habe das Skript angepasst. Jetzt kannst du die Farbe auswählen und
-;	transparenz an/aus-schalten.
-;
-;	Lars
-;
-;
-;
 ; This program is free software; you can redistribute it and/or modify
 ; it under the terms of the GNU General Public License as published by
 ; the Free Software Foundation; either version 2 of the License, or
@@ -27,7 +13,7 @@
 ; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 
-(define (script-fu-twab img drawable text font:name fcolor bcolor transparent)
+(define (script-fu-twab img drawable text text2 font:name fcolor bcolor transparent)
 
   (let* (
 	 (width (car (gimp-image-width img)))
@@ -35,54 +21,59 @@
 
 	 (font:size (/ height 45))
 
+	 (text2-not-empty (> (string-length text2) 0))
+	 (hfactor (if text2-not-empty 2.0 1.0))
+
 	 (bg:width width)
-	 (bg:height (* font:size 1.6))
+	 (bg:height (* font:size 1.6 hfactor))
 	 (bg:x 0)
 	 (bg:y (- height bg:height))
 
-	 (text:x font:size)
-	 (text:y (- height (* font:size 1.5)))
+	 (text:height font:size)
+	 (text:x text:height)
+
+	 (text:y (- height (* text:height 1.5 hfactor)))
+	 (text:y2 (- height (* text:height 1.5)))
 
 	 (layer:bg (car (gimp-layer-new img width height RGBA-IMAGE "background-layer" 
 					(opacity transparent) 
 					NORMAL-MODE)))
 	 (layer:text (car (gimp-layer-new img width height RGBA-IMAGE "text-layer" 100 NORMAL-MODE)))
+	)
 
-
-	 )
-
-;    (gimp-image-undo-disable img)
-
-					; add transparent background and text layer
+; add transparent background and text layer
     (gimp-image-add-layer img layer:bg 0)
     (gimp-image-add-layer img layer:text 0)
 
-					; add background
-    (gimp-rect-select img bg:x bg:y bg:width bg:height REPLACE 0 0)
+; add background
+    (gimp-rect-select img bg:x bg:y bg:width (* bg:height hfactor) REPLACE 0 0)
     (gimp-palette-set-background bcolor)
     (gimp-edit-fill layer:bg BG-IMAGE-FILL)
     (gimp-selection-none img)
 
-					; add text
+; add text
     (gimp-context-set-foreground fcolor)
     (gimp-floating-sel-anchor 
      (car (gimp-text-fontname img layer:text text:x text:y 
 			      text 0 TRUE font:size PIXELS font:name)))
 
-					; flatten image (save wouldn't ask to export image)
+; add second text, if not empty
+    (if text2-not-empty
+	(gimp-floating-sel-anchor 
+	 (car (gimp-text-fontname img layer:text text:x text:y2 
+			      text2 0 TRUE font:size PIXELS font:name))))
+
+; flatten image (save wouldn't ask to export image)
     (gimp-image-flatten img)
 
-;    (gimp-image-undo-enable img)
-
     (gimp-displays-flush)
-    )
-  )
+))
+
 
 (define (opacity transparent)
-(if 
-					 (= transparent TRUE) 
-					 50 
-					 100) )
+  (if (= transparent TRUE)
+      50 
+      100))
 
 
 (script-fu-register "script-fu-twab"
@@ -90,14 +81,14 @@
 		    "Text with Background"
 		    "Lars Gregori gimp/at/beidfarbig/de"
 		    "Lars Gregori"
-		    "2014-02-01"
+		    "2014-06-21"
 		    "RGB*"
 		    SF-IMAGE "image" 0
 		    SF-DRAWABLE "layer" 0
 		    SF-STRING "text" ""
+		    SF-STRING "text2" ""
 		    SF-FONT "font:name" "Sans"
-		    SF-COLOR "color" '(255 255 255)
+		    SF-COLOR "font:color" '(255 255 255)
 		    SF-COLOR "background color" '(0 0 0)
 		    SF-TOGGLE "transparent" FALSE
 		    )
-
